ef28d2ed2cc0e3f749adcfcce87f5f578c602425
leo11-senai <leonardo.m.junior11@aluno.senai.br>
2026-03-11 11:46:48 -0300

    11.03

---
+++ 09.03/primeiro_objeto.js
@@ -0,0 +1,20 @@
+class conta {
+    constructor(tipo,titular,saldo){
+        this.tipo= tipo;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+    mostrarextrato(){
+        return `A conta ${this.tipo} do ${this.titular} tem ${this.saldo} reais`
+        
+    }
+    depositar(valor){
+        this.saldo= this.saldo + valor;
+    }
+}
+
+let contaCorrente = new conta ("Corrente","João",10000);
+let ContaPoupanca = new conta ("Poupança","Maria",50000)
+console.log(contaCorrente.mostrarextrato());
+contaCorrente.depositar(1500);
+console.log(contaCorrente.mostrarextrato())
