967d8bc3c12f1dc97aca457c8cc12f981ebeb85f
joabefg <joabmartinsefg@outlook.com>
2026-03-15 22:10:48 -0300

    Projeto completo

---
+++ projetos/projeto_rpg/jogo_rpg_copy.js
@@ -0,0 +1,15 @@
+class Personagem {
+    constructor(nome, titulo, hp, mana, energia) {
+        this.nome = nome;
+        this.titulo = titulo;
+        this.hp = hp;
+        this.mana = mana;
+        this.energia = energia;
+    }
+
+    atacar(alvo, habilidade) {
+
+        return `Mensagem do turno`;
+    }
+}
+
