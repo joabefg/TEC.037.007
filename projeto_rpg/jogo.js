9c3b6fa88371f5e57e16dd3dc6d0b90ed8202fd1
Henrique <henriqueduartemartinscmd@gmail.com>
2026-03-27 12:12:27 -0300

    27.03

--- projeto_rpg/jogo.js
+++ projeto_rpg/jogo.js
@@ -1,50 +1,125 @@
 class Personagem {
-    constructor(nome, titulo, hp, mana, energia){
+    constructor(nome, titulo, hp, mana, energia) {
         this.nome = nome;
-        this.hp = hp;
-        this.mana = mana;
-        this.energia = energia;
         this.titulo = titulo;
+        this.hp = hp;
+        this.hpMax = hp;
+        this.mana = mana;
+        this.manaMax = mana;
+        this.energia = energia;
+        this.energiaMax = energia;
     }
-    atacar(alvo, habilidades){
+    atacar(alvo, habilidade) {
+        if (!habilidade) return `${this.nome} não fez nada.`;
 
+        if (this.mana >= habilidade.custo
+            && this.energia >= habilidade.energia) {
+            alvo.hp -= habilidade.dano;
+            if (alvo.hp < 0) alvo.hp = 0;
+
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo;
+                this.energia += 50;
+                if (this.energia > this.energiaMax) this.energia = this.energiaMax;
+            }
+
+            if (habilidade.energia > 0) {
+                this.energia -= habilidade.energia;
+                if (this.energia < 0) this.energia = 0;
+            }
+
+            return `${this.nome} usou ${habilidade.nome}! (-${habilidade.dano} HP)`;
+
+        } else {
+            return `${this.nome} sem mana/energia para usar ${habilidade.nome}!`;
+        }
+    }
+    boss_atacar(alvo){
+        if(this.energia == 300){
+            alvo.hp -= 15 ;
+            this.energia = 0;
+            return `Boss usou sua habilidade`
+        } else {
+            this.energia +=50;
+            return `Boss carregou o ataque`
+        }
     }
 }
- class habilidade {
 
-    constructor(id, nome, dano, custo, energia){
+class Habilidade {
+    constructor(id, nome, dano, custo, energia) {
         this.id = id;
         this.nome = nome;
+        this.dano = dano;
         this.custo = custo;
         this.energia = energia;
-        this.dano = dano;
-
-        
     }
- }  
-let hero = new Personagem("Ector", "O Herói", 100, 100, 0);
-let boss =  new Personagem("Astrogildo", "Vilão", 100, 0, 50);
+}
 
-document.getElementById("nome-heroi").textContent = 
-  `⚔️${hero.nome}`;
-document.getElementById("titulo-heroi").textContent = 
-  `${hero.titulo}`;
+// 
 
-  document.getElementById("nome-boss").textContent = 
-  `⛏️${boss.nome}`;
-document.getElementById("titulo-boss").textContent = 
-  `${boss.titulo}`;
+let hero = new Personagem("Naruto Uzumaki", "O futuro Hokage", 400, 300, 800);
+let boss = new Personagem("Sasuke Uchiha", "O Último Uchiha", 400, 300, 0);
 
-  let container = document.getElementById("controles");
-  let listaHabilidades = [
-  new Habilidade(1, "ATTACK", 4, 0, 0),
-  new Habilidade(2, "skill", 8, 10, 0),
-  new Habilidade(3, "ULTIMATE", 15, 0, 100)
-  ];
-  listaHabilidades.array.forEach(hab => {
+console.log(boss.hp)
+document.getElementById("nome-heroi").textContent = hero.nome;
+document.getElementById("titulo-heroi").textContent = "🐅 " + hero.titulo;
+
+document.getElementById("nome-boss").textContent = boss.nome;
+document.getElementById("titulo-boss").textContent = "👁️‍🗨️ " + boss.titulo;
+
+let constructorn = document.getElementById("Controles");
+const turnos = ["Aguardar a Ação"];
+
+const atualizarInterface = (msg_hero, msg_boss) => {
+    //Barra herói
+    document.getElementById("hp-hero").value = hero.hp;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("energia-hero").value = hero.energia;
+    //Barra Boss
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("energia-boss").value = boss.energia;
+    
+    //turnos
+    document.getElementById("log-hero").textContent = msg_hero;
+    document.getElementById("log-boss").textContent = msg_boss;
+
+    if (boss.hp <= 0) {
+        document.getElementById("tela").innerHTML = "Naruto Uzumaki wins!"
+    }
+    if (hero.hp <= 0) {
+        document.getElementById("tela").innerHTML = "Sasuke"
+    }
+}
+
+
+let container = document.getElementById("controles");
+container.innerHTML = "";
+
+let listaHabilidades = [
+    new Habilidade(1, "Attack", 4, 0, 0),
+    new Habilidade(2, "Skill", 8, 10, 0),
+    new Habilidade(3, "Ultimate", 15, 0, 100)
+];
+
+
+
+function desabilitarBotoes() {
+    document.querySelectorAll("#controles button").forEach(btn => btn.disabled = true);
+}
+
+listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");
-    btn.innerText = hab.nome;
-    btn.classList.add("btn", "btn-warning")
+    btn.innerHTML = hab.nome;
+    btn.classList.add("btn", "btn-dark")
     container.appendChild(btn);
-  });
- 
+
+    btn.onclick = () => {
+        let msg_hero = hero.atacar(boss, hab);
+
+       let msg_boss = boss.boss_atacar(hero)
+
+        atualizarInterface(msg_hero, msg_boss);
+    }
+
+})
