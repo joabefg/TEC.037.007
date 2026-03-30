62f2cd4302b5e35f015aa3bc10aa3486e15d901b
andrevbrz-coder <andrevbrz@gmail.com>
2026-03-30 12:17:07 -0300

    att

---
+++ app_02/helloword/src/app/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native'
+
+export default function Index() {
+    return (
+        <view>
+            <text>Hello</text>
+            <text>World</text>
+        </view>
+    )
+}                                                                                                                                                                
