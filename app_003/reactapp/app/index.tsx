bc307ed59208e21f7b8715461e134ab1253bc652
Eduardo Augusro <Sarahlayane4@gmail.com>
2026-04-06 12:16:13 -0300

    app004

--- app_003/reactapp/app/index.tsx
+++ app_003/reactapp/app/index.tsx
@@ -1,4 +1,5 @@
 import { View,StyleSheet,Image, Text } from 'react-native';
+import { Link } from 'expo-router';
 
 const estilo = StyleSheet.create({
     conteiner : {
@@ -12,6 +13,27 @@ const estilo = StyleSheet.create({
         fontWeight: 'bold',
         color: 'rgb(277,277,277)',
         
+    },
+    subtitulo:{
+         fontSize: 25,
+        fontWeight: 'bold',
+        color: 'rgb(193,193,196)',
+        textAlign: 'center',
+    },
+    botao:{
+        fontSize:17,
+        backgroundColor:'rgb(88,196,220)',
+        color: 'rgb(27, 27, 29)',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    btnGroup:{
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        gap: 10,
+        marginTop: 20,
+
     }
 })
 
@@ -20,7 +42,11 @@ export default function Index(){
         <View style={estilo.conteiner}>
             <Image source={require("@/assets/react.svg")} />
             <Text style={estilo.titulo}> React Native</Text>
-            <Text> Aprenda uma vez, escreva em qualquer lugar </Text>
+            <Text style={estilo.subtitulo}> Aprenda uma vez, escreva em qualquer lugar </Text>
+            <View style={estilo.btnGroup}>
+                <Link href='/' style={estilo.botao}>Começando</Link>
+                <Link href='/' style={estilo.botao}>Documentos</Link>
+            </View>
         </View>
     )
 }
