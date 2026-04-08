4a473a73319b2d683ba93e899a615189081b828a
Marcos Paulo Alves Borges <tenn77wolf@gmail.com>
2026-04-08 12:03:14 -0300

    Update input.tsx

--- app04/login/src/components/input.tsx
+++ app04/login/src/components/input.tsx
@@ -1,7 +1,15 @@
-import { TextImput } from "../../components/Themed";
+import { TextInput, StyleSheet,TextInputProps } from "react-native"
 
-export function Input() {
-  return (
-    <TextImput></><TextInput/>
-  );
+export function Input({...rest}: TextInputProps) {
+    return <TextInput style={styles.input} {...rest} />
 }
+const styles = StyleSheet.create({
+    input: {
+        width: '100%',
+        height: 48, 
+        borderWidth: 1,
+        borderColor: '#DCDCDC',
+        borderRadius: 8,
+        marginTop: 5
+    }
+})
