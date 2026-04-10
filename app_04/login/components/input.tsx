bc90e09be77573563cbc7eba50c290888e8c185b
Victor <matheus.v.freitas9@aluno.senai.br>
2026-04-10 07:46:47 -0300

    app 04

--- app_04/login/components/input.tsx
+++ app_04/login/components/input.tsx
@@ -4,11 +4,12 @@ export function Input () {
     return <TextInput />
 }
 const style = StyleSheet.create({
-    input: {
-        width: 100%,
-        height: 48,
+    input: {                         
+        width: '100%',
+        height: 48,   
         borderWidth: 1,
         borderColor: '#DCDCDC',
         borderRadius: 8,
+        
     }
 })
