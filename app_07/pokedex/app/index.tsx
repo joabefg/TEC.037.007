25cf37e6e527d7a7a3f2dc9a5025f1cadbd96b76
Joab Martins <joabmartinsefg@outlook.com>
2026-04-15 09:57:30 -0300

    pokedex 01

---
+++ app_07/pokedex/app/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+export default function App() {
+    interface Pokemon {
+        name: string;
+        url: string;
+    }
+
+    const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+    useEffect(() => {
+        buscarPokemons()
+    }, [])
+
+    async function buscarPokemons() {
+        const resposta = await fetch(
+            "https://pokeapi.co/api/v2/pokemon/?limit=10")
+            const data = await resposta.json();
+            console.log(data)
+            setPokemons(data.results);
+    }
+
+    return (
+        <ScrollView>
+            {pokemons.map((pokemon) => (
+                <View key={pokemon.name}>
+                    <Text>{pokemon.name}</Text>
+                </View>
+            ))}
+        </ScrollView>
+    )
+}
