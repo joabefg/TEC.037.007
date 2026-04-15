c6c021cd91e2370a2c31df3070f1d8bd45261b34
Sarah Layane <Sarahlayane4@gmail.com>
2026-04-15 09:59:39 -0300

    app007

---
+++ app_007/pokedex/app/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+export default function app(){
+    interface Pokemon{
+        name: string;
+        url: string;
+    }
+    const [pokemons,setPokemons] = useState<Pokemon[]>([])
+    useEffect(() =>{
+        buscarPokemons()
+    }, [])
+    async function buscarPokemons(){
+        const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+            const data = await resposta.json();
+        console.log(data)
+        setPokemons(data.results);
+    }
+
+    return (
+        <ScrollView>
+            {pokemons.map((pokemon) => (
+                <View key={pokemon.name}>
+                    <Text>{pokemon.name}</Text>
+                </View>
+            ) )}
+        </ScrollView>
+    )
+
+
+}
+
