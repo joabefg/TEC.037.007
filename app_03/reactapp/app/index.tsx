6103d895ba7a0e21f101e120aa094b528c5daec1
João Freitas Hosken Cunha <jfhcunha@gmail.com>
2026-04-17 10:44:04 -0300

    app07

---
+++ app_03/reactapp/app/index.tsx
@@ -0,0 +1,16 @@
+import {View, StyleSheet} from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+        </View>
+    );
+}
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rrgb(255, 255, 255)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
