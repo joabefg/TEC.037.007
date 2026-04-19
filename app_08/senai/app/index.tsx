eee40a7298ef819410d05edd4ae83cdadc630c6d
Vitória G. <vitoriasantosgoncalves10466@gmail.com>
2026-04-19 16:12:42 -0300

    atividade avaliativa

--- app_08/senai/app/index.tsx
+++ app_08/senai/app/index.tsx
@@ -1,17 +1,43 @@
-import  React  from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
-export default function Index () {
-    return(
-        <View style={styles.container}>
-        <Text style={estilos.title}>Bem-vindo ao Senai!</Text>
-        <Link style={estilos.button} href="/detalhes">Sobre</Link>
-        </View>
-    )
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={require('../assets/logo_senai.png')} 
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>Bem-vindo ao Senai!</Text>
+      <Link href="../sobre" style={styles.button}> Sobre </Link>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-    container:{
-        flex: 1, 
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  image: {
+    width: 140,
+    height: 140,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  button: {
+  backgroundColor: '#0b36ac',
+  color: '#fff',          // cor do texto
+  padding: 10,
+  borderRadius: 8,
+  textAlign: 'center',
+  marginTop: 10,
+},
+});
--- app_08/senai/app/sobre.tsx
+++ app_08/senai/app/sobre.tsx
@@ -1,25 +1,43 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { Link } from 'expo-router';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Sobre() {
-    const router = useRouter();
-    return (
-        <View style={styles.container}>
-            <Text>Sobre</Text>
-            <Link href="/index" style={estilos.button}>Sobre</Link>
-            <Button title='Realizar logout'
-            onPress={() => {
-                router.dismissAll();
-                router.replace("/index")
-            }}></Button>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sobre</Text>
 
-        </View>
-    )
+      <Text style={styles.texto}>
+        O Serviço Nacional de Aprendizagem Industrial (SENAI) tem como objetivo estimular 
+        a inovação industrial por meio da educação, 
+        consultoria, pesquisa aplicada e serviços técnicos e tecnológicos que são decisivos para
+        a competitividade das empresas do Estado e do Brasil.
+        É um dos cincos maiores complexos de educação profissional do mundo e o maior da América Latina e reconhecido como modelo de educação profissional, pela qualidade dos serviços
+       que promove e pela aposta em formatos educacionais diferenciados e inovadores,
+       que capacitam profissionais em cursos presenciais e a distância, formando trabalhadores aptos a contribuir para o desenvolvimento da indústria.
+Diretoria
+Nossas
+Unidades
+      </Text>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 20,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+
+  texto: {
+    textAlign: 'flex-start',
+    fontSize: 16,
+  },
+});
